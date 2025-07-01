89cc87594b33ec199030c80acfb0cde65c45280b
askar <adusadus498@gmail.com>
2025-07-02 00:58:23 +0200

    Update start.py
    Modification du logging

--- start.py
+++ start.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
 import discord
-import time
-import math
 from discord.ext import commands
-import logging
+"""import logging"""
 
-logging.basicConfig(level=logging.INFO)
+"""logging.basicConfig(level=logging.INFO)"""
 
 
 load_dotenv()
@@ -16,12 +14,15 @@ class MyBot(commands.Bot):
     async def setup_hook(self):
         for extension in ['random','ping','mimir','poke','sun','xp_system','youtube']:
             await self.load_extension(f'cogs.{extension}')
-            logging.info(f'Loaded: cogs.{extension}')
+            print(f'Loaded: cogs.{extension}')
+
+            """logging.info(f'Loaded: cogs.{extension}')"""
     async def on_ready(self):
         await bot.tree.sync()
+        print(f'Lancé en tant que {self.user} !' + 'Version "discord.py": {discord.__version__}')
         """await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name =f"{bot.command_prefix}help"))"""
-        logging.info(f'Lancé en tant que {self.user} !')
-        logging.info(f"discord.py version: {discord.__version__}")
+        """logging.info(f'Lancé en tant que {self.user} !')"""
+        """logging.info(f"discord.py version: {discord.__version__}")"""
         
 
 intents = discord.Intents.all()
